95071d8d69f44d9fff6bb8f4441db02b42b522fc
Bateomar <omar12012004@hotmail.fr>
2025-06-18 10:44:12 +0200

    dans les javascript
    const API_BASE_URL = 'http://localhost:8888/ProjetCommun-main/backend';

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost/ProjetCommun/backend';
+const API_BASE_URL = 'http://localhost:8888/ProjetCommun-main/backend';
 let previewChartInstance;
 
 async function apiRequest(endpoint, options = {}) {
